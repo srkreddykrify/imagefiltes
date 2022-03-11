eeeb290c30e94c155db6847b1b7fc2978021852c
Srkreddy <srkreddy@krify.net>
2022-03-11 15:45:03 +0530

    Initial Commit

---
+++ imagefilter/src/main/java/com/user/imagefilter/ImageFilterMessage.java
@@ -0,0 +1,15 @@
+package com.user.imagefilter;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ImageFilterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+
+}
